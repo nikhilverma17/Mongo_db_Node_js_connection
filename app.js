c243ce15e2dc9e2a30da76adfe00b5fcdeb5294f
Nikhil Verma <iamvermanikhil@gmail.com>
2023-06-15 10:07:07 +0530

    main

---
+++ app.js
@@ -0,0 +1,52 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+async function main() {
+  const uri = "mongodb+srv://iamvermanikhil:burger20@newdatabase.iy7zrhl.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB Atlas');
+
+    // New Database Created
+    const db = client.db('fruitsDB');
+
+    //Adding Collection in database
+    const fruitsCollection = db.collection('fruits');
+
+    // Insert a single document
+    // const fruit = { _id: 1, name: 'Mango', Price: '30' };
+    // const result = await fruitsCollection.insertOne(fruit);
+    // console.log('Inserted document:', result.insertedId);
+
+    //  Find all documents
+    // const allFruits = await fruitsCollection.find().toArray();
+    // console.log('All fruits:', allFruits);
+    //  Find documents matching a condition
+    // const redFruits = await fruitsCollection.find({ color: 'Red' }).toArray();
+    // console.log('Red fruits:', redFruits);
+    
+    
+    // Update a single document
+    // const result = await fruitsCollection.updateOne(
+    //   { name: 'Mango' },
+    //   { $set: { price: '50' } }
+    // );
+    // console.log('Updated document count:', result.modifiedCount);
+    
+    
+    // Delete a single document
+    // const result = await fruitsCollection.deleteOne({ name: 'Mango' });
+    // console.log('Deleted document count:', result.deletedCount);
+
+    // Perform other operations here
+
+  } catch (error) {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  } finally {
+    await client.close();
+    console.log('Connection closed');
+  }
+}
+
+main().catch(console.error);
